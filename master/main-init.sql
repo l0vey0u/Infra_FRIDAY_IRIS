13920cdc59b4670060ea8615aaee3abbdca6fb48
l0vey0u <duckhoim@kyonggi.ac.kr>
2020-10-15 22:21:00 +0900

    security(haproxy) : let haproxy work only at local

--- master/main-init.sql
+++ master/main-init.sql
@@ -5,7 +5,7 @@ create user 'slave_at_ms'@'%' identified by 'Iz0ne!!!!';
 create user 'haproxy'@'172.20.1.2';
 grant replication slave on *.* to 'slave_at_mm_sub'@'%' with grant option;
 grant replication slave on *.* to 'slave_at_ms'@'%' with grant option;
-GRANT USAGE,REPLICATION CLIENT,PROCESS,SHOW DATABASES,SHOW VIEW ON *.* TO 'haproxy'@'%';
+GRANT USAGE,REPLICATION CLIENT,PROCESS,SHOW DATABASES,SHOW VIEW ON *.* TO 'haproxy'@'172.20.1.2';
 flush privileges;
 CHANGE MASTER TO MASTER_HOST='sub_master_db', MASTER_USER='slave_at_mm_main', MASTER_PASSWORD='Iz0ne!!!!', MASTER_AUTO_POSITION=1;
 START SLAVE;
